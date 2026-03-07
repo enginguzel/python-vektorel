93d7e4325e861d0b9514eddbd9665aadaaabc650
enginguzel <enginmertguzel@gmail.com>
2026-03-07 12:24:21 +0300

    07.03.26

--- fonk4_degerdonduren.py
+++ fonk4_degerdonduren.py
@@ -1,8 +1,14 @@
 def topla(a,b):
-    retun f"toplam: {a+b}"
+    return f"toplam: {a+b}"
 print(topla(5,6))
 
 def topla(a,b):
     return f"toplam: {a+b}"
 a = topla(5,6)
 print(a)
+
+
+def selamla(k):
+    return f"merhaba {k}, nasilsin {k}"
+selamla("sevilay")
+print(selamla("sevilay"))
